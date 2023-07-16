0b71f411197cb219b03c67eb7475199cc92ed618
barathkumarg <yibarat1@gmail.com>
2023-07-16 11:58:24 +0530

    multimodal-maven

---
+++ MavenProject/web-app/src/main/java/com/spring/UserController.java
@@ -0,0 +1,14 @@
+package com.spring;
+
+
+//importing the dependency from the other maven maven module 
+
+// these can be done after compling and jar creation of the source project
+public class UserController {
+	
+	User fetchinfo (UserController data) {
+		
+		return new User();
+	}
+
+}
